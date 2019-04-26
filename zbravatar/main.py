8b58c049a0e451f5e5e56a2bb2d97407de274613
Plrerolle <plrerolle@gmail.com>
2019-04-26 10:49:24 +0200

    Adding zbravatar generation

---
+++ zbravatar/main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from random import random
+import seaborn as sns
+
+def generate_zbravatar_pattern():
+    rd_half_zbravatar = np.random.randint(2, size=(7, 4))
+    zbravatar_pattern = np.concatenate((rd_half_zbravatar, np.flip(rd_half_zbravatar[:, :3], axis=1)), axis=1)
+    return zbravatar_pattern
+
+def generate_zbravatar(i):
+    zbravatar_pat = generate_zbravatar_pattern()
+
+    plt.imshow(zbravatar_pat, cmap=sns.cubehelix_palette(rot=-random(), as_cmap=True, gamma=2))
+    plt.axis('off')
+    plt.savefig('../zbraa/src/assets/imgs/zbravatar_{}.png'.format(i))
+
+
+if __name__ == "__main__":
+    generate_zbravatar(2)
