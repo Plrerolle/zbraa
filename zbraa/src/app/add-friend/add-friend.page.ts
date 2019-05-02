459d6d7d10c3ec50212ad30dd6fae05ba2e96dc8
Plrerolle <plrerolle@gmail.com>
2019-05-02 14:54:54 +0200

    Modifying zbravatar creation : hopefully this will work

--- zbraa/src/app/add-friend/add-friend.page.ts
+++ zbraa/src/app/add-friend/add-friend.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {UserCreateService} from '../services/user-create.service'
+import { UserCreateService } from '../services/user-create.service'
+
 
 @Component({
   selector: 'app-add-friend',
@@ -8,25 +9,26 @@ import {UserCreateService} from '../services/user-create.service'
 })
 export class AddFriendPage {
   results: null;
+  zbravatarId: 0;
   zbravatarResults: null;
-  userData =  {};
+  userData = {};
 
   constructor(
     private createService: UserCreateService
   ) { }
 
+
   createUser() {
     console.log(this.userData);
     this.createService.createUser(this.userData).subscribe(data => {
-      if (data !== null) {
-        this.results = data
-        this.createService.createZbravatar({id: this.results === null ? 0 : this.results.id, zbra_path: '/home/plrerolle/zbraa/zbraa/src/assets/imgs/'}).subscribe(data => {
-        this.zbravatarResults = data
-      });
-      }
-      
+      this.results = data;
+      this.createService.createZbravatar({ id: data.id, zbra_path: '/home/plrerolle/zbraa/zbraa/src/assets/imgs/' })
+        .subscribe(zbraData => {
+          this.zbravatarResults = zbraData;
+        });
+
     });
-    
+
     console.log(this.zbravatarResults);
 
   }
